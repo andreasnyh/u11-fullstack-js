c81026160cd8177b7f60ccf2e0c3cf31a4c28cf9
Andreas Ny <andreas.nyh87@gmail.com>
2020-09-17 22:01:18 +0200

    Lint working, maybe..

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -2,45 +2,40 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    es6: true,
+    es6: true
   },
   parserOptions: {
     sourceType: 'module',
-    ecmaVersion: 2020,
+    ecmaVersion: 2018,
     ecmaFeatures: {
-      jsx: true,
-    },
+      jsx: true
+    }
   },
   extends: [
-    'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
     'airbnb',
-    'plugin:prettier/recommended',
+    'prettier',
+    'prettier/react',
+    'plugin:prettier/recommended'
   ],
-  plugins: ['react', 'simple-import-sort'],
+  plugins: ['react', 'prettier', 'simple-import-sort'],
   settings: {
     react: {
-      version: 'detect', // Automatically detect the react version
+      version: 'detect' // Automatically detect the react version
     },
+    'import/resolver': {
+      node: { moduleDirectory: ['node_modules', 'src/'] }
+    }
   },
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
   rules: {
     'prettier/prettier': ['error', {}, { usePrettierrc: true }],
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
     'simple-import-sort/sort': 'error',
-    'jsx-a11y/anchor-is-valid': [
-      'error',
-      {
-        components: ['Link'],
-        specialLink: ['hrefLeft', 'hrefRight'],
-        aspects: ['invalidHref', 'preferButton'],
-      },
-    ],
-  },
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }]
+  }
 };
