a455e1a86c0c3f9f25bedc92784fa8e9a677d4e0
Andreas Ny <andreas.nyh87@gmail.com>
2020-09-17 19:14:46 +0200

    Linting seems to work

---
+++ client/.eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', 'simple-import-sort'],
+  settings: {
+    react: {
+      version: 'detect', // Automatically detect the react version
+    },
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  rules: {
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'simple-import-sort/sort': 'error',
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
+    ],
+  },
+};
