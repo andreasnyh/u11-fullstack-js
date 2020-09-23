879236361e6d4adf862da998730a2e5b9184191e
Andreas Ny <andreas.nyh87@gmail.com>
2020-09-23 12:19:00 +0200

    Create Button and Nav components Add file containing color variables

---
+++ client/src/components/Button.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+import Colors from '../config/Colors';
+
+const Button = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${(props) => (props.lightMode ? Colors.DarkBlue : Colors.Pink)};
+  color: ${(props) => (props.lightMode ? Colors.Black : Colors.White)};
+  font-size: 1rem;
+  margin: 1em;
+  padding: 0.6em 2em;
+  border: 0;
+  border-radius: 8px;
+  &&:hover {
+    background: ${Colors.White};
+    color: ${Colors.Black};
+  }
+`;
+
+export default Button;
---
+++ client/src/config/Colors.js
@@ -0,0 +1,13 @@
+const Colors = {
+  White: '#FFFFFF',
+  Pink: '#E94774',
+  Black: '#000000',
+  Beige: '#F5ECEA',
+  LightBlue: '#4F7E8F',
+  DarkBlue: '#304E59',
+  Yellow: '#EAB93D',
+  Green: '#9EB87D',
+  LightGray: '#E5E5E5'
+};
+
+export default Colors;
