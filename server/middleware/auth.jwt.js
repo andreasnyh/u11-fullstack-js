306d4508e468603d7a8bff15edc78972195f97c9
Andreas Ny <andreas.nyh87@gmail.com>
2020-09-25 18:21:08 +0200

    Add isAdmin function

--- server/middleware/auth.jwt.js
+++ server/middleware/auth.jwt.js
@@ -1,10 +1,8 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
-// const User = require('../models/user');
+const { User, Role } = require('../models');
 
 const verifyToken = (req, res, next) => {
-  // const bearerHeader = req.headers.authorization;
-  // const token = bearerHeader && bearerHeader.split(' ')[1];
   const token = req.headers['x-access-token'];
 
   if (!token) return res.status(403).send({ message: 'No token provided' });
@@ -13,16 +11,19 @@ const verifyToken = (req, res, next) => {
     if (err) {
       return res.status(401).send({
         message: 'Unauthorized!',
+        reason: err.message,
       });
     }
-    req.userId = decoded;
+    req.userId = decoded.id;
     return next();
   });
 };
 
-/* const isAdmin = (req, res, next) => {
+const isAdmin = (req, res, next) => {
+  // Find the user in db
   User.findById(req.userId).then((user) => {
-    user. ().then((roles) => {
+    // Does the user have admin role assigned?
+    Role.find({ _id: { $in: user.roles } }, (err, roles) => {
       for (let i = 0; i < roles.length; i++) {
         if (roles[i].name === 'admin') {
           next();
@@ -31,10 +32,10 @@ const verifyToken = (req, res, next) => {
       }
 
       res.status(403).send({
-        message: 'Require Admin Role!',
+        message: 'Requires Admin Role!',
       });
     });
   });
-}; */
+};
 
-module.exports = verifyToken;
+module.exports = { verifyToken, isAdmin };
