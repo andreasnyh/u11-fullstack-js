0a6311aa8d5f0fc1845914d6f30869dc7088c31b
Andreas Ny <andreas.nyh87@gmail.com>
2020-09-25 18:16:47 +0200

    Update imports and set headers

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const authController = require('../controllers/auth.controller');
 const { validate } = require('../middleware/validate');
-const { checkEmailExists } = require('../middleware/verify.email.role');
+const { verify } = require('../middleware');
 const {
   validateUserCreate,
   validateUserLogin,
@@ -12,20 +12,20 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-/* app.use((req, res, next) => {
+app.use((req, res, next) => {
   res.header(
     'Access-Control-Allow-Headers',
     'x-access-token, Origin, Content-Type, Accept',
   );
   next();
-}); */
+});
 
 // check('what in req', 'message if error').typeOfCheck()
 app.post('/signin', validate(validateUserLogin), authController.signIn);
 
 app.post(
   '/signup',
-  [validate(validateUserCreate), checkEmailExists],
+  [validate(validateUserCreate), verify.checkEmailExists],
   authController.signUp,
 );
 
--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,27 +1,35 @@
 const express = require('express');
 const userController = require('../controllers/user.controller');
 const notFound = require('../controllers/notfound.controller');
-const verifyToken = require('../middleware/auth.jwt');
+const { authJwt } = require('../middleware');
 
 const router = express();
+
+router.use((req, res, next) => {
+  res.header(
+    'Access-Control-Allow-Headers',
+    'x-access-token, Origin, Content-Type, Accept',
+  );
+  next();
+});
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: false }));
 
-router.get('/', verifyToken, userController.index);
-router.get('/find/:email', verifyToken, userController.detail);
+router.get('/', authJwt.verifyToken, userController.index);
+router.get('/find/:email', authJwt.verifyToken, userController.detail);
 
-router.post('/find', verifyToken, userController.detail);
+router.post('/find', authJwt.verifyToken, userController.detail);
 
 router.get('/test/all', userController.allAccess);
 
-router.get('/test/user', verifyToken, userController.userBoard);
+router.get('/test/user', authJwt.verifyToken, userController.userBoard);
 
 router.get(
   '/test/admin',
   [
-    verifyToken,
-    //  authJwt.isAdmin
+    authJwt.verifyToken,
+    authJwt.isAdmin,
   ],
   userController.adminBoard,
 );
