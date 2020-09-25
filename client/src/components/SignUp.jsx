8f91899189fa07b72d8b21b854b40bf84ea04ff5
Andreas Ny <andreas.nyh87@gmail.com>
2020-09-25 09:35:37 +0200

    Add eventHandlers to SignUp Component.

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -15,25 +15,58 @@ const StyledSignUp = styled.div`
   border-radius: 30px 30px 0 0;
 `;
 
-const SignUp = () => {
-  return (
-    <StyledSignUp>
-      <h2>Sign Up Component</h2>
-      <Form text="Register">
-        <Input type="text" name="firstName" placeholder="First name" />
-        <Input type="text" name="lastName" placeholder="Last name" />
-        <Input type="email" name="email" placeholder="E-mail" />
-        <Input type="text" name="password" placeholder="Password" />
-        <Input type="text" name="passwordAgain" placeholder="Repeat password" />
-      </Form>
-      <Link to="/">
-        <Button lightMode>Back</Button>
-      </Link>
-      <Link to="/">
-        <Button lightMode>Register</Button>
-      </Link>
-    </StyledSignUp>
-  );
-};
+class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(JSON.stringify(this.state, null, 2));
+  }
+
+  render() {
+    return (
+      <StyledSignUp>
+        <h2>Sign Up Component</h2>
+        <Form text="Register">
+          <Input
+            type="text"
+            name="firstName"
+            placeholder="First name"
+            onChange={this.handleChange}
+          />
+          <Input type="text" name="lastName" placeholder="Last name" onChange={this.handleChange} />
+          <Input type="email" name="email" placeholder="E-mail" onChange={this.handleChange} />
+          <Input type="text" name="password" placeholder="Password" onChange={this.handleChange} />
+          <Input
+            type="text"
+            name="passwordAgain"
+            placeholder="Repeat password"
+            onChange={this.handleChange}
+          />
+        </Form>
+        <Link to="/">
+          <Button lightMode>Back</Button>
+        </Link>
+        {/* <Link to="/"> */}
+        <Button type="submit" lightMode onClick={this.handleSubmit}>
+          Register
+        </Button>
+        {/* </Link> */}
+      </StyledSignUp>
+    );
+  }
+}
 
 export default SignUp;
