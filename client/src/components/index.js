07380592c1c25a78147e33e8da0bf9415783eb07
Andreas Ny <andreas.nyh87@gmail.com>
2020-09-29 17:26:59 +0200

    Add Home and index components

---
+++ client/src/components/index.js
@@ -0,0 +1,8 @@
+import Home from './Home';
+import MainNav from './MainNav';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import SignUpThankYou from './SignUpThankYou';
+import Welcome from './Welcome';
+
+export { Home, MainNav, SignIn, SignUp, SignUpThankYou, Welcome };
