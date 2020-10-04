8a874baf6551621bfdf974dc3e60df20b8903b17
Andreas Ny <andreas.nyh87@gmail.com>
2020-10-04 17:21:22 +0200

    Add image to Welcome

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { Redirect, useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 
+import image from '../assets/illustrations/meeting_splash.png';
 import { Colors } from '../config/ColorsShadows';
-import Button from './elements/Button';
+import { Button } from './elements';
 import SplashImage from './elements/SplashImage';
 
 const StyledWelcome = styled.div`
@@ -21,13 +22,34 @@ const StyledWelcome = styled.div`
   flex-direction: column;
 `;
 
+const StyledContainer = styled.div`
+  width: 100vw;
+  height: 60vh;
+  overflow: hidden;
+  position: relative;
+`;
+
+const StyledText = styled.h1`
+  width: 100vw;
+  background-color: ${Colors.Dark};
+  opacity: 0.7;
+  margin-top: 5rem;
+  padding: 0.5rem 0;
+`;
+
 const Welcome = () => {
   const history = useHistory();
+  const currentUser = localStorage.getItem('currentUser');
+  if (currentUser !== null) {
+    return <Redirect to="/home" />;
+  }
   return (
     <>
-      <SplashImage imgUrl="https://via.placeholder.com/414x414?text=Splash+image+placeholder" />
+      <StyledContainer>
+        <StyledText>Welcome!</StyledText>
+        <SplashImage imgUrl={image} />
+      </StyledContainer>
       <StyledWelcome>
-        <h2>Welcome Component!</h2>
         {/* Margins to center buttons vertically */}
 
         <Button
--- client/src/components/elements/SplashImage.jsx
+++ client/src/components/elements/SplashImage.jsx
@@ -3,10 +3,12 @@ import styled from 'styled-components';
 const splashImage = styled.img`
   top: 0;
   left: 0;
-  z-index: -1;
-  width: 100%;
-  height: 100%;
+  z-index: 1;
+  min-width: 100%;
+  min-height: 100%;
   position: absolute;
+  transform: scale(1.01);
+  background-size: contain;
   background-position: top;
   background-repeat: no-repeat;
   background-image: url(${(props) => props.imgUrl});
