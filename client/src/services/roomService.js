406db02ce309b0db0a21d00a6389a56fc5556976
Andreas Ny <andreas.nyh87@gmail.com>
2020-10-05 21:45:11 +0200

    Add createRoom function in roomService

--- client/src/services/roomService.js
+++ client/src/services/roomService.js
@@ -4,6 +4,52 @@ import axios from 'axios';
 import config from '../config/config.json';
 import { authHeader, handleResponse } from '../helpers';
 
+async function createRoom(room) {
+  axios
+    .post('http://localhost:5000/api/auth/AddRoom', room)
+    .then((res) => {
+      console.log(res);
+      console.log(res.data);
+      // window.location = `${window.location.origin}/home`;
+    })
+    .catch((error) => {
+      if (error.response) {
+        const errorArray = [];
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        error.response.data.errors.forEach((err) => {
+          console.log(
+            'status:',
+            error.response.status,
+            '\nparam:',
+            err.param,
+            '\nError:',
+            err.msg
+          );
+          errorArray.push({ param: err.param, mgs: err.msg });
+        });
+
+        console.log(errorArray);
+        this.setState({
+          errors: errorArray
+        });
+
+        // console.log(error.response.status);
+        // console.log(error.response.data);
+        // console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+      // console.log(error.config);
+    });
+}
+
 async function getAll() {
   const header = await authHeader();
   return axios
@@ -79,6 +125,7 @@ async function getOne(id) {
 }
 
 const roomService = {
+  createRoom,
   getAll,
   getOne
 };
