98b55b73b5fe656a5d0e880589026c23d83ba08a
Andreas Ny <andreas.nyh87@gmail.com>
2020-10-05 16:58:58 +0200

    Fix some styling to elements

--- client/src/components/elements/Card.jsx
+++ client/src/components/elements/Card.jsx
@@ -5,7 +5,7 @@ import { Colors, Shadows } from '../../config/ColorsShadows';
 const Card = styled.div`
   width: 100%;
   padding: 1em;
-  margin: 0 auto;
+  margin: 1rem auto 2rem;
   position: relative;
   border-radius: 20px;
   color: ${Colors.lightText};
