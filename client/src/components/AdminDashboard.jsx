47ec693a53e812ede0a4b2c6a1385bd4ca00fb31
Andreas Ny <andreas.nyh87@gmail.com>
2020-11-24 08:08:45 +0100

    WIP Admin dashboard

--- client/src/components/AdminDashboard.jsx
+++ client/src/components/AdminDashboard.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import { Button, Card, CardFull, Text } from './elements';
+
+const DashButton = styled(Button)`
+  width: 8rem;
+  height: 8rem;
+  margin: 0;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-gap: 3rem;
+  margin: 4rem 0;
+  justify-items: center;
+  grid-template-columns: 1fr 1fr;
+`;
 
 const AdminDashboard = () => {
   return (
-    <>
-      <h1>AdminDashboard</h1>
-      <p>lots of functions</p>
-    </>
+    <CardFull>
+      <Card>
+        <Text headline="AdminDashboard" />
+        <Grid>
+          <DashButton>Users</DashButton>
+          <DashButton>Rooms</DashButton>
+          <DashButton>Add Room</DashButton>
+        </Grid>
+      </Card>
+    </CardFull>
   );
 };
 
