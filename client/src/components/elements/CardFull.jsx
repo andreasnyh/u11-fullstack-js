de58fbbb7cadd4bfd480a55547924732ad56993a
Andreas Ny <andreas.nyh87@gmail.com>
2020-12-02 17:51:32 +0100

    Fix styling, limit width

--- client/src/components/elements/CardFull.jsx
+++ client/src/components/elements/CardFull.jsx
@@ -5,19 +5,21 @@ import Card from './Card';
 
 const CardFull = styled.div`
   bottom: 0;
-  top: 4.4rem;
+  /* top: 4.4rem; */
   width: 100%;
+  padding: 1em;
   display: flex;
   margin: 0 auto;
-  margin-top: ${(props) => (props.static ? '4.4rem' : '0')};
-  padding: 1em;
-  position: ${(props) => (props.static ? 'static' : 'absolute')};
+  max-width: 700px;
   align-items: center;
   color: ${Colors.Text};
   height: calc(100% - 21px);
   border-radius: 30px 30px 0 0;
   background-color: ${Colors.Dark};
+  margin-top: ${(props) => (props.static ? '4rem' : '0')};
   flex-direction: ${(props) => (props.row ? 'row' : 'column')};
+  position: ${(props) => (props.static ? 'static' : 'absolute')};
+
   ${Card}:last-child {
     margin-bottom: 4rem;
   }
