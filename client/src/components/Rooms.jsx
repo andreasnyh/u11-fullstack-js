640848b0cb0bc128d84559a17e4c8a6c221e2a78
Andreas Ny <andreas.nyh87@gmail.com>
2020-12-05 15:45:40 +0100

    Add filtering to Rooms

--- client/src/components/Rooms.jsx
+++ client/src/components/Rooms.jsx
@@ -1,24 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
-import { RoomList } from './elements';
+import { Form, Input, Label, RoomList, Text } from './elements';
 
 const bookRoom = (id) => {
   console.log(id);
 };
 
+const SearchLabel = styled(Label)`
+  text-align: left;
+  margin-bottom: 0;
+  margin-left: 3rem;
+  font-weight: 700;
+`;
+
 const Rooms = (props) => {
   const { rooms } = props;
+  const [query, setQuery] = useState('');
+  const [filteredRooms, setFilteredRooms] = useState(null);
 
-  return rooms.map((room) => {
-    return (
-      <RoomList
-        key={room._id}
-        room={room}
-        // moreInfo={moreInfo}
-        bookRoom={bookRoom}
-      />
-    );
-  });
+  // Filter users
+  useEffect(() => {
+    if (!rooms) return;
+    if (!query) setFilteredRooms(rooms);
+    const regexp = new RegExp(query.toLowerCase(), 'g');
+    const oldRooms = rooms.map((room) => room);
+
+    const roomSize = Number(query);
+
+    const newRooms = oldRooms.filter((room) => {
+      return room.name.toLowerCase().match(regexp) ||
+        room.town.toLowerCase().match(regexp) ||
+        room.street.toLowerCase().match(regexp) ||
+        (roomSize >= room.size.from && roomSize <= room.size.to)
+        ? room
+        : null;
+    });
+    setFilteredRooms(newRooms);
+  }, [query, rooms]);
+
+  return (
+    <>
+      <Form handleSubmit={(event) => event.preventDefault()}>
+        <SearchLabel style={{ maxWidth: '500px' }}>Filter Rooms</SearchLabel>
+        <Input
+          type="text"
+          onChange={(event) => setQuery(event.target.value)}
+          style={{ maxWidth: '500px' }}
+        />
+      </Form>
+
+      {filteredRooms && filteredRooms.length === 0 && (
+        <Text headlineSub="No rooms matching search" />
+      )}
+      {filteredRooms &&
+        filteredRooms.map((room) => {
+          return (
+            <RoomList
+              key={room._id}
+              room={room}
+              // moreInfo={moreInfo}
+              bookRoom={bookRoom}
+            />
+          );
+        })}
+    </>
+  );
 };
 
 export default Rooms;
